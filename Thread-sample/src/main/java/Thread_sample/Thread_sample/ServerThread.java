d0fae367692c95472f19748f2a2d068bbc468e0a
Avinash Tiwari <qwe123kids@gmail.com>
2018-03-11 19:31:15 -0600

    Thread Sleep

--- Thread-sample/src/main/java/Thread_sample/Thread_sample/ServerThread.java
+++ Thread-sample/src/main/java/Thread_sample/Thread_sample/ServerThread.java
@@ -13,7 +13,12 @@ public class ServerThread extends Thread {
 		int clinent = 1;
 		while (clinent != 100) {
 			System.out.println(this.getName() + " send data to client" + clinent);
-			
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			clinent = clinent +1;
 		}
 	}
