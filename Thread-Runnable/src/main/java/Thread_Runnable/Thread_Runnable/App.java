bb474aede6c5bdda3787bdc986dc92059c2a9fcd
Avinash Tiwari <qwe123kids@gmail.com>
2018-03-11 20:29:17 -0600

    Runable

--- Thread-Runnable/src/main/java/Thread_Runnable/Thread_Runnable/App.java
+++ Thread-Runnable/src/main/java/Thread_Runnable/Thread_Runnable/App.java
@@ -8,6 +8,16 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	ImplementRunable runnable = new ImplementRunable(1);
+    	Thread thread = new Thread(runnable);
+    	thread.start();
+    	ImplementRunable runnabl2 = new ImplementRunable(2);
+    	Thread thread2 = new Thread(runnable);
+    	thread2.start();
+    	System.out.println(thread2.getName());
+    	System.out.println(thread.getName());
+    	ImplementRunable runnabl3 = new ImplementRunable(3);
+    	runnabl3.start();
+  
     }
 }
