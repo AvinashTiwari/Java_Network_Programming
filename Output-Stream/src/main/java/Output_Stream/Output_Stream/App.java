722b8c4efe1f5a31eb9991b6d7e8f3c40ad6853b
Avinash Tiwari <qwe123kids@gmail.com>
2018-03-13 19:48:21 -0600

    Output Straem

---
+++ Output-Stream/src/main/java/Output_Stream/Output_Stream/App.java
@@ -0,0 +1,34 @@
+package Output_Stream.Output_Stream;
+
+import java.io.FileOutputStream;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public static void main(String [] args)
+    {
+        try
+        {
+            FileOutputStream fileOutput = new FileOutputStream("example2.txt", false);
+            
+            char H = 'H';
+            
+            fileOutput.write((char)H);
+            
+            String string = "Hello OutputStream!";
+            
+            fileOutput.write(string.getBytes());
+            
+            fileOutput.close();
+            
+            
+        }
+        catch(Exception e)
+        {
+            System.out.println(e.toString());
+        }
+    }
+}
