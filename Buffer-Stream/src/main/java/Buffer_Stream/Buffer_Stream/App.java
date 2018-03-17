7d21f09ba06cac305b5007e967ac789b52228496
Avinash Tiwari <qwe123kids@gmail.com>
2018-03-16 23:45:33 -0600

    buffer stream

---
+++ Buffer-Stream/src/main/java/Buffer_Stream/Buffer_Stream/App.java
@@ -0,0 +1,33 @@
+package Buffer_Stream.Buffer_Stream;
+
+/**
+ * Hello world!
+ *
+ */
+import java.io.*;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	 try
+         {
+             BufferedReader reader = new BufferedReader(new FileReader("example6.txt"), 1024);
+             BufferedWriter writer = new BufferedWriter(new FileWriter("example7.txt"));
+             
+             String line = null;
+             
+             while((line = reader.readLine()) != null)
+             {
+                 writer.write(line);
+                 writer.newLine();
+             }
+             writer.close();
+             reader.close();
+         }
+         catch(Exception e)
+         {
+             System.err.println(e.toString());
+         }
+    }
+}
