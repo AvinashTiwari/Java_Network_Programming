5e3a6bed999bf5ff8c910916dac7784bc2a9859a
Avinash Tiwari <qwe123kids@gmail.com>
2018-03-20 23:08:33 -0600

    Internet Address

---
+++ Internet-Address/src/main/java/Internet_Address/Internet_Address/App.java
@@ -0,0 +1,23 @@
+package Internet_Address.Internet_Address;
+
+/**
+ * Hello world!
+ *
+ */
+import java.io.*;
+import java.net.*;
+
+public class App {
+	public static void main(String[] args) {
+		try {
+			InetAddress address = InetAddress.getLocalHost();
+			System.out.println(address.getHostAddress());
+			System.out.println(address.getHostName());
+			InetAddress address2 = InetAddress.getByName("google.com");
+			System.out.println(address2.getHostAddress());
+			System.out.println(address2.getHostName());
+		} catch (IOException e) {
+			System.out.println(e.toString());
+		}
+	}
+}
