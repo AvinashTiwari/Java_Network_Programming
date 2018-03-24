62e5d8a89ba82e14675f40cc61ad0bda23109ac8
Avinash Tiwari <qwe123kids@gmail.com>
2018-03-24 17:54:17 -0600

    Ip validation

---
+++ Ip-validation/src/main/java/Ip_validation/Ip_validation/App.java
@@ -0,0 +1,39 @@
+package Ip_validation.Ip_validation;
+
+import org.apache.commons.validator.routines.InetAddressValidator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		InetAddressValidator validator = new InetAddressValidator();
+		String ipAddress = "192.168.0.321";
+		boolean isValid = validator.isValid(ipAddress);
+		if (isValid) {
+			System.out.println(ipAddress + " is valid");
+		} else {
+			System.out.println(ipAddress + " is not valid");
+		}
+	}
+	
+	 public boolean validateIpAddress(String ipAddress)
+	    {
+	        String[] numbers = ipAddress.split("\\.");
+	        if (numbers.length != 4)
+	        {
+	            return false;
+	        }
+	        
+	        for(String str: numbers)
+	        {
+	            int i = Integer.parseInt(str);
+	            if((i<0) || (i>255)) 
+	            {
+	                return false;
+	            }
+	        }
+	        return true;
+	    }
+}
