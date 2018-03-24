047d40156fbf1645294b3320318fd597fc1c4d6c
Avinash Tiwari <qwe123kids@gmail.com>
2018-03-23 23:05:55 -0600

    ping

---
+++ Ping/src/main/java/Ping/Ping/App.java
@@ -0,0 +1,54 @@
+package Ping.Ping;
+
+/**
+ * Hello world!
+ *
+ */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.InetAddress;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	try
+        {
+            String hostAddress = "www.google.com";
+            InetAddress host = InetAddress.getByName(hostAddress);
+            System.out.println(host.isReachable(1000));
+
+            
+            Process p = Runtime.getRuntime().exec("ping " + hostAddress);
+            BufferedReader inputStream = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+            String commandOutput = "";
+            boolean isReachable = true;
+            // reading output stream of the command
+            while ((commandOutput = inputStream.readLine()) != null) {
+                //System.out.println(commandOutput);
+                
+                if(commandOutput.contains("Destination host unreachable"))
+                {
+                    isReachable = false;
+                    break;
+                }
+                        
+            }
+            
+            if(isReachable)
+            {
+                System.out.println("Host is reachable!");
+            }
+            else
+            {
+                System.out.println("Host is not reachable!");
+            }
+            
+        
+
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+    }
+}
